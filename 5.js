f1857c77488e111e5067ffbdeb14a3dd403a52a2
rroyson <frontiersons@gmail.com>
2017-06-06 16:07:56 -0400

    Rob Commit

--- 5.js
+++ 5.js
@@ -2,3 +2,21 @@ const utils = require('./fp-utils')
 
 // filters
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function print(v) {
+  console.log(v)
+}
+
+function isEven(v) {
+  return  divisibleBy(2)(v)
+}
+
+function divisibleBy(number) {
+  return function (v) {
+    return v % number === 0
+  }
+}
+
+const newList = utils.filter(isEven, numbers)
+
+utils.forEach(print, newList)
